911d7099664f393321b3aa41537f5459ce281108
Otomo kento <otmknt@gmail.com>
2024-05-11 15:59:55 +0900

    修正しました。

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default {
       // headerTopic:"お問い合わせ"
       headerTopic:["トップページ","会社案内","お問い合わせ","事業紹介","掲示板","会社実績"],
       // footerTopic:{contact:"お問い合わせ",aboutUs:"私たちの活動",diary:"プログラミング日記",result:"開発実績"}
-      barnerSentence:"当サイトにお越しいただき、誠にありがとうございます",
+      barnerSentence:"当サイトにお越しいただき、誠にありがとうございます.",
       setNewsList:[{date:"2022/05/13",category:"お知らせ",title:"ブログを作成いたしました"},{date:"2022/11/16",category:"お知らせ",title:"ブログを更新いたしました"}]
       //router-viewにpropするのは大丈夫でした
     }
